19b31d5c410d2145d0d2d2c2700a6189ae0dac18
Jo Pol <jo-pol@users.noreply.github.com>
2022-09-28 13:13:46 +0200

    Add files via upload

---
+++ release-assets.sh
@@ -0,0 +1,5 @@
+zip -q lace_circular_ground.zip lace_circular_ground.*
+zip -q lace_ground.zip lace_ground.*
+zip -q lace_grid.zip lace_grid.*
+zip -q lace_polar.zip lace_polar.*
+cd templates && zip -r -q ../templates.zip *
